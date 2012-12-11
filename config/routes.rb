9294ba075be97a1a6d7e5bab9e4fdc6ed9299e08
virtual-force <nabeel.nizami@virtual-force.com>
2012-12-11 15:30:09 +0500

    changed

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ Sweet::Application.routes.draw do
   devise_for :users, :controllers=> { :omniauth_callbacks=> "users/omniauth_callbacks"}
 
 
-  devise_for :users
-   devise_scope :user do
-    get "sign_in", :to => "users/sessions#new"
-    get "sign_out", :to => "users/sessions#destroy"
-  end
+#  devise_for :users
+#   devise_scope :user do
+#    get "sign_in", :to => "users/sessions#new"
+#    get "sign_out", :to => "users/sessions#destroy"
+#  end
 #  get "sessions/new"
 #
 #  get "log_out" => "sessions#destroy", :as => "log_out"
