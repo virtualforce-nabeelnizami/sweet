210300100bd44bde2894b2dce4429a46dac78a3f
virtual-force <nabeel.nizami@virtual-force.com>
2012-12-11 14:45:05 +0500

    changed

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   # The following is for facebook
 #  provider :facebook, [APP ID], [SECRET KEY], {:scope =&gt; 'email, read_stream, read_friendlists, friends_likes, friends_status, offline_access'}
 #  config.omniauth :facebook, "APP_ID", "APP_SECRET”
- config.omniauth :facebook, '232566186876778', '513a68a19c539ae02332b8eb954fcc97'
+# config.omniauth :facebook, '232566186876778', '513a68a19c539ae02332b8eb954fcc97'
   # If you want to also configure for additional login services, they would be configured here.
 end
