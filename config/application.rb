77f50c59c24aa1a9380d124b12d7ec24c9406157
virtual-force <nabeel.nizami@virtual-force.com>
2012-12-11 12:42:41 +0500

    changed

--- config/application.rb
+++ config/application.rb
@@ -54,8 +54,9 @@ module Sweet
     config.active_record.whitelist_attributes = true
 
     # Enable the asset pipeline
+   
+    config.assets.initialize_on_precompile = false
     config.assets.enabled = true
-    config.assets.initialize_on_precompile = true
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
